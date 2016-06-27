6f41f4ced9c6729464a77c07e47e9bcf11803659
Kaitlin Saunders <kaitlinhsaunders@gmail.com>
2016-06-27 08:46:04 -0400

    .

--- js/app.js
+++ js/app.js
@@ -1,3 +1,13 @@
+//populate information to page
+$('appt.html').on('load', function (e){
+  e.preventDefault
+$('.title').text(date[tempIndex].title);//what is the actual name for this in the object?
+$('.street').text(date[tempIndex].cityState);//what is the actual name for this in the object?
+$('.date').text(date[tempIndex].date);
+$('.time').text(date[tempIndex].time);
+console.log("Appointment info run");
+});
+
 //load previous page
 $('.left-arrow').on('click', function (e){
   e.preventDefault
@@ -5,16 +15,16 @@ $('.left-arrow').on('click', function (e){
 });
 
 
-//Delete appointment from the local storage
+//Delete appointment from the local storage (WITHOUT THE ACTUAL FUNCTIONALITY)
 $('.deleteButton').on('click', function (e){
   e.preventDefault
   var confirmDelete = confirm("Are you sure you want to delete this appointment?");
   if (confirmDelete == true) {
-      // x = "Appointment has been deleted";
-      //Tell local storage to delete item
+      window.location = 'index.html';
   } else {
   }
-  window.location = 'index.html';
+
+
 });
 
 
@@ -22,4 +32,10 @@ $('.saveButton').on('click', function(e){
   e.preventDefault
   alert("Your changes have been saved");
   window.location = 'index.html';
+});
+
+
+$('.editAppointment').on('click', function (e){
+  e.preventDefault
+  window.location = 'appt.html'
 });
