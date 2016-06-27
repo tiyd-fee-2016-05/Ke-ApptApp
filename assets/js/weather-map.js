0706b7c93ac3c19c45a51bdd70dfb901974e2bba
Erik Petersen <contacterikpetersen@gmail.com>
2016-06-26 23:00:55 -0400

    Cleaned up code and commented and credited

--- assets/js/weather-map.js
+++ assets/js/weather-map.js
@@ -1,27 +1,32 @@
 $( function() {
+  
   "use strict";
 
   $( document ).ready( function() {
     console.log( JSON.parse(localStorage.getItem("appointments")));
-
     console.log( JSON.parse(localStorage.getItem("tempIndex")));
+    // grab the index that was clicked on in index.html
     var tempIndex = JSON.parse(localStorage.getItem("tempIndex"));
-
+    // grab the data in localStorage and separate into object
     var date = JSON.parse( localStorage.getItem("appointments").split() );
+    // populate input fields on page with specific object data
     $( "h1" ).text( date[tempIndex].title );
     $( ".appointment-heading" ).text( date[tempIndex].date + " @ " );
     $( ".appointment-time" ).text( date[tempIndex].time );
+    $( "figcaption" ).text( date[tempIndex].cityState );
   }); // end document.ready
 
+  // arrow at top takes you back to index.html
   $('.left-arrow').on('click', function (e){
-  e.preventDefault
-  window.location = 'index.html';
-});
+    e.preventDefault
+    window.location = 'index.html';
+  });
 
-$('.edit').on('click', function (e){
-e.preventDefault
-window.location = 'appt.html';
-});
+  // edit button at bottom takes you to appt.html
+  $('.edit').on('click', function (e){
+    e.preventDefault
+    window.location = 'appt.html';
+  });
 }) // end outmost function
 
 
